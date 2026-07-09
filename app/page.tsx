2fb8c7cb7bf43abcf992fd262456550911e4f811
cineverseai4-pixel <cineverseai4@gmail.com>
2026-07-09 21:17:31 +0530

    Add home  page

---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+export default function Home() {
+
+  return (
+
+    <main
+
+      style={{
+
+        minHeight: "100vh",
+
+        background: "#0f172a",
+
+        color: "white",
+
+        display: "flex",
+
+        justifyContent: "center",
+
+        alignItems: "center",
+
+        flexDirection: "column",
+
+        fontFamily: "Arial",
+
+      }}
+
+    >
+
+      <h1>CineVerse AI 🎬</h1>
+
+      <p>Your AI Movie & Web Series Assistant</p>
+
+    </main>
+
+  );
+
+}
